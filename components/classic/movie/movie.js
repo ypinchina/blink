2f968299a59f531448e2db1b64e957974744a964
ypinchina <batmanyp@foxmail.com>
2021-03-07 22:52:13 +0800

    构建movie组件UI与数据联调

---
+++ components/classic/movie/movie.js
@@ -0,0 +1,30 @@
+// components/classic/movie/movie.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    image: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
