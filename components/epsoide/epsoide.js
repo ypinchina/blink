338aa6950ca57c79e8652a827df8d80e1c3d2228
ypinchina <batmanyp@foxmail.com>
2021-03-08 22:27:00 +0800

    完成首页header组件布局，第八章完成

--- components/epsoide/epsoide.js
+++ components/epsoide/epsoide.js
@@ -4,17 +4,34 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index: Number
+    index: {
+      type:Number,
+      observer: function(newVal, oldVal, path) {
+        newVal = newVal < 10 ? '0' + newVal : newVal
+        this.setData({
+          _index: newVal
+        })
+      }
+    }
   },
-
+  attached() {
+    let year = new Date().getFullYear()
+    let monthIndex = new Date().getMonth()
+    let months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+    this.setData({
+      year: year,
+      month: months[monthIndex]
+    })
+  },
   /**
    * 组件的初始数据
    */
   data: {
     year: 0,
-    month: ''
+    month: '',
+    _index: ''
   },
-  
+
   /**
    * 组件的方法列表
    */
