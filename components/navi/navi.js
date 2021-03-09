a52895398693dcd0c91703897f42c5c13d18b6fc
yip <@yip>
2021-03-09 09:49:23 +0800

    ‘navi组件的布局架构，文件与数据联调 样式设置’

---
+++ components/navi/navi.js
@@ -0,0 +1,28 @@
+// components/navi/navi.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+    first: Boolean, // 是否是第一期
+    latest: Boolean // 是否是最新一期,与first互逆
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    leftImg: './images/triangle@left.png',
+    rightImg: './images/triangle@right.png',
+    disLeft: './images/triangle.dis@left.png',
+    disRight: './images/triangle.dis@right.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
