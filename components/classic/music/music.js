a4daf3b006581ecc490c6dd7158ea8385071585c
yip <@yip>
2021-03-12 18:54:44 +0800

    音乐组件封装，播放

--- components/classic/music/music.js
+++ components/classic/music/music.js
@@ -1,24 +1,41 @@
 import classicBeh from '../classic-beh'
+const gBgM = wx.getBackgroundAudioManager()
 Component({
   /**
    * 组件的属性列表
    */
   behaviors: [classicBeh],
   properties: {
+    musicSrc: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: '',
-    playSrc: ''
+    playStatus: false, // 播放状态 ，默认是未播放状态
+    pauseSrc: './images/player@pause.png',
+    playSrc: './images/player@play.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay(event) {
+      if (!this.data.playStatus) {
+        this.setData({
+          playStatus: true
+        })
+        gBgM.src = this.properties.musicSrc
+        console.log(gBgM.src)
+        console.log(this.properties.musicSrc)
+      } else {
+        gBgM.pause()
+        this.setData({
+          playStatus: false
+        })
+      }
+    }
   }
 })
