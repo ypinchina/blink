24cb31740b56974df989709e75d57dc892c6d7ab
ypinchina <batmanyp@foxmail.com>
2021-03-24 22:18:41 +0800

    输入框搜索和点击标签搜索联调

--- components/search/search.js
+++ components/search/search.js
@@ -13,7 +13,10 @@ Component({
    */
   data: {
     historyList: [],
-    hotList: []
+    hotList: [],
+    searching: false,
+    q: '',
+    bookList: []
   },
   attached() {
     this.setData({
@@ -29,22 +32,35 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    clearSearch1() {
+      console.log(1)
+      // 点击搜索框的叉按钮，清空搜索框
+      this.setData({
+        searching: true,
+        q: ''
+      })
+    },
     onCancel() {
       this.triggerEvent('cancel', {})
     },
     submitSearch(event) {
-      const inputVal = event.detail.value
-      
+      const inputVal = event.detail.value || event.detail.content
       if (!inputVal) {
         return
       }
+      this.setData({
+        searching: true,
+        q: inputVal
+      })
       keywordModel.searchSubmit({
         'q': inputVal,
         'summary': 1,
         'start': 0
       }).then(res => {
         keywordModel.setHistory(inputVal)
-        console.log(res.data)
+        this.setData({
+          bookList: res.data.books
+        })
       })
     }
   }
